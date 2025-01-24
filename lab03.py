53cbe335381a06f82d5b628f8d249728e8f56945
Fahmid <fahmid.alshahriar21@gmail.com>
2025-01-24 10:55:57 -0500

    This is lab03

--- lab03.py
+++ lab03.py
@@ -1,1 +1,6 @@
-print ("Hello world")
+# Dice Options
+diceOptions = list(range(1, 7))
+
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
+
+print("Available Weapons: ", ', '.join(weapons))
